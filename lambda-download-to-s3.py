aef7a27a0573070628eeb3c4c3ce2d0a766597b8
Sean Boots <sean.boots@gmail.com>
2021-12-30 15:25:00 -0500

    Separate download and S3-upload logic

--- lambda-download-to-s3.py
+++ lambda-download-to-s3.py
@@ -5,12 +5,16 @@
 # https://stackoverflow.com/a/32490661
 # https://towardsdatascience.com/serverless-functions-and-using-aws-lambda-with-s3-buckets-8c174fd066a1
 
+# Attempt 2
+# https://stackoverflow.com/a/42254534
+
 import os
 import json
 import boto3
-import botocore.vendored.requests.packages.urllib3 as urllib3
 import logging
 from botocore.exceptions import ClientError
+import urllib.request
+import shutil
 from datetime import datetime
 
 URL = os.environ['url']  # URL of the site to download, stored in the site environment variable
@@ -18,24 +22,40 @@ BUCKET = os.environ['destination_bucket']
 FOLDER = os.environ['destination_folder']
 FILENAME = os.environ['destination_file']
 
-def generate_filename():
+def generate_bucket_filename():
     date = datetime.today().strftime('%Y-%m-%d')
     folder = FOLDER
     filename = FILENAME
     return f'{folder}/{date}/{filename}'
 
+def generate_tmp_filename():
+    filename = FILENAME
+    return f'/tmp/{filename}'
+
+
+# Thanks to https://stackoverflow.com/a/7244263/756641
+
+def download_to_filename(url, file_name):
+    # Download the file from `url` and save it locally under `file_name`:
+    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+
 def download_and_upload():
     url = URL  # put your url here
     bucket = BUCKET  # your s3 bucket
-    key = generate_filename()  # your desired s3 path or filename
+    key = generate_bucket_filename()  # your desired s3 path or filename
+
+    s3 = boto3.resource('s3')
 
     print(f'Downloading from {url}')
+    
+    download_to_filename(url,generate_tmp_filename())
+
     print(f'Saving to bucket: {bucket} in {key}')
 
-    s3=boto3.client('s3')
-    http=urllib3.PoolManager()
     try:
-        s3.upload_fileobj(http.request('GET', url,preload_content=False), bucket, key)
+        s3.meta.client.upload_file(generate_tmp_filename(), bucket, key)
     except ClientError as e:
         logging.error(e)
         return False
